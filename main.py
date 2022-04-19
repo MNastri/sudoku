509c524bd3a9e1542b19c6a08f7f2695d81bbf8b
MNastri <marcellonastri@gmail.com>
2022-04-19 16:26:11 -0300

    feat:get game board

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 import pygame as pg
+import requests
 
 
 def _draw_grid_lines(display):
     for ii in range(10):
-        start = end = screen_pad + ii * cell_side_length
+        start = end = scr_pad + ii * cell_length
         # Horizontal lines
         pg.draw.line(
             display,
             line_color,
-            (screen_pad, start),
-            (screen_side_length - screen_pad, end),
+            (scr_pad, start),
+            (scr_length - scr_pad, end),
         )
         # Vertical lines
         pg.draw.line(
             display,
             line_color,
-            (start, screen_pad),
-            (end, screen_side_length - screen_pad),
+            (start, scr_pad),
+            (end, scr_length - scr_pad),
         )
 
 
-cell_side_length = 60
-screen_pad = 30
-screen_side_length = screen_pad * 2 + cell_side_length * 9
+def _get_board():
+    uri = "https://sugoku.herokuapp.com/board?difficulty=easy"
+    response = requests.get(uri)
+    return response.json()["board"]
+
+
+cell_length = 60
+scr_pad = 30
+scr_length = scr_pad * 2 + cell_length * 9
 line_color = (255, 255, 255)
 
 pg.init()
-display = pg.display.set_mode((screen_side_length, screen_side_length))
+display = pg.display.set_mode((scr_length, scr_length))
 _draw_grid_lines(display=display)
+board = _get_board()
 run = True
 while run:
     for event in pg.event.get():
