6a35a0f5f86eb71b27e7c88af15c7326e0cab911
MNastri <marcellonastri@gmail.com>
2022-04-19 16:57:29 -0300

    feat:draw numbers on board

--- main.py
+++ main.py
@@ -22,16 +22,35 @@ def _get_board():
     return response.json()["board"]
 
 
+def _draw_numbers(board, font, display):
+    for line_num,line in enumerate(board):
+        for column_num,num in enumerate(line):
+            if num == 0:
+                continue
+            img = font.render(str(num), True, number_color)
+            img_rect = img.get_rect(
+                center=(
+                    scr_pad + column_num * cell_length + cell_length // 2,
+                    scr_pad + line_num * cell_length + cell_length // 2,
+                )
+            )
+            display.blit(img, img_rect)
+
+
 cell_length = 60
 scr_pad = 30
 scr_length = scr_pad * 2 + cell_length * 9
 line_color = (255, 255, 255)
+number_color = (0, 0, 200)
+font_size = 30
 
 pg.init()
 display = pg.display.set_mode((scr_length, scr_length))
+number_font = pg.font.SysFont("Garamond", font_size)
 pg.display.set_caption("Sudoku")
 _draw_grid_lines(display=display)
 board = _get_board()
+_draw_numbers(board=board, font=number_font, display=display)
 run = True
 while run:
     for event in pg.event.get():
